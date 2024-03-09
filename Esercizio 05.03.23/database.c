ee7af4c0b08ef18420a965f65f4378dbc1ca4eef
Lxlp <66387301+Lxlp38@users.noreply.github.com>
2024-03-09 20:49:59 +0100

    implemented multi record on each IndexNode
    now each IndexNode can store multiple Persona entries, handled bu a PersonaNode linked list

--- Esercizio 05.03.23/database.c
+++ Esercizio 05.03.23/database.c
@@ -3,6 +3,17 @@
 #include <string.h>
 
 
+/***
+ *      _____        _          _______                    
+ *     |  __ \      | |        |__   __|                   
+ *     | |  | | __ _| |_ __ _     | |_   _ _ __   ___  ___ 
+ *     | |  | |/ _` | __/ _` |    | | | | | '_ \ / _ \/ __|
+ *     | |__| | (_| | || (_| |    | | |_| | |_) |  __/\__ \
+ *     |_____/ \__,_|\__\__,_|    |_|\__, | .__/ \___||___/
+ *                                    __/ | |              
+ *                                   |___/|_|              
+ */
+
 typedef struct Persona {
     char name[20];
     char surname[50];
@@ -24,11 +35,16 @@ typedef struct DataStructure {
     Dato value;
 } DataStructure;
 
+typedef struct PersonaNode {
+    Persona* persona;
+    struct PersonaNode* next;
+} PersonaNode;
+
 // The following struct is able to store both type of datas (char* and int)
 // By also storing the related record, it is possible to fetch it with more simplicity
 typedef struct IndexNode {
     DataStructure* data;
-    Persona* record;
+    PersonaNode* records;
     struct IndexNode* left;
     struct IndexNode* right;
 } IndexNode;
@@ -50,6 +66,16 @@ Persona* findByAge(Database * database, int age);
 ////
 
 
+/***
+ *      _____        _         _____ _                   _                     ____                       _   _                 
+ *     |  __ \      | |       / ____| |                 | |                   / __ \                     | | (_)                
+ *     | |  | | __ _| |_ __ _| (___ | |_ _ __ _   _  ___| |_ _   _ _ __ ___  | |  | |_ __   ___ _ __ __ _| |_ _  ___  _ __  ___ 
+ *     | |  | |/ _` | __/ _` |\___ \| __| '__| | | |/ __| __| | | | '__/ _ \ | |  | | '_ \ / _ \ '__/ _` | __| |/ _ \| '_ \/ __|
+ *     | |__| | (_| | || (_| |____) | |_| |  | |_| | (__| |_| |_| | | |  __/ | |__| | |_) |  __/ | | (_| | |_| | (_) | | | \__ \
+ *     |_____/ \__,_|\__\__,_|_____/ \__|_|   \__,_|\___|\__|\__,_|_|  \___|  \____/| .__/ \___|_|  \__,_|\__|_|\___/|_| |_|___/
+ *                                                                                  | |                                         
+ *                                                                                  |_|                                         
+ */
 // Function needed to translate raw data types into a DataStructure, needed for pseudo-polymorphism, and used inside most other functions.
 DataStructure* datafy(Dato dato, DataType type){
     DataStructure* data = malloc(sizeof(DataStructure));
@@ -59,21 +85,12 @@ DataStructure* datafy(Dato dato, DataType type){
 }
 // Compares the value of two DataStructures.
 // The DataType must be the same, or else an error is thrown.
-// If the first value is greater than the second, a value >0 is returned. Else, <0 is returned
-int dataCompare(DataStructure* a, DataStructure* b){
-    if (a->type != b->type){
-        perror("Data Types do not match!");
-    }
-    if (a->type == STRING){
-        return strcmp(a->value.stringa,b->value.stringa);
-    }
-    return a->value.numero > b->value.numero ? 1 : 0;
-}
-int dataCompareRaw(DataStructure* a, Dato b){
+// If the first value is greater than the second, a value >0 is returned. Else, <0 is returned. If the data are equals, 0 is returned.
+int dataCompare(DataStructure* a, Dato b){
     if (a->type == STRING){
         return strcmp(a->value.stringa,b.stringa);
     }
-    return a->value.numero > b.numero ? 1 : 0;
+    return a->value.numero > b.numero ? 1 : ( a->value.numero == b.numero ) ? 0 : -1;
 }
 //Checks if the value of a DataStructure and of a raw Dato are the same.
 int dataEquals(DataStructure* a, Dato b){
@@ -83,17 +100,48 @@ int dataEquals(DataStructure* a, Dato b){
     return (a->value.numero == b.numero);
 }
 
+
+
+
+/***
+ *      _   _           _             _    _                 _ _ _             
+ *     | \ | |         | |           | |  | |               | | (_)            
+ *     |  \| | ___   __| | ___  ___  | |__| | __ _ _ __   __| | |_ _ __   __ _ 
+ *     | . ` |/ _ \ / _` |/ _ \/ __| |  __  |/ _` | '_ \ / _` | | | '_ \ / _` |
+ *     | |\  | (_) | (_| |  __/\__ \ | |  | | (_| | | | | (_| | | | | | | (_| |
+ *     |_| \_|\___/ \__,_|\___||___/ |_|  |_|\__,_|_| |_|\__,_|_|_|_| |_|\__, |
+ *                                                                        __/ |
+ *                                                                       |___/ 
+ */
+
+
+PersonaNode* createPersonaNode(Persona* p){
+    PersonaNode* pn = malloc(sizeof(PersonaNode));
+    pn->persona = p;
+    pn->next = NULL;
+    return pn;
+}
+
 // Creates a node from a root_value, and the record (Persona) it is referencing
 IndexNode* createNode(DataStructure* root_value, Persona* p){
     IndexNode* root = malloc(sizeof(IndexNode));
     if (root == NULL) {
         return NULL;
     }
+    PersonaNode* personanode = createPersonaNode(p);
     root->data = root_value;
-    root->record = p;
+    root->records = personanode;
     root->left = NULL;
     root->right = NULL;
     return root;
+}
+
+
+PersonaNode* addPersonaNode(PersonaNode* root, Persona* p){
+    if (root == NULL){
+        return createPersonaNode(p);
+    }
+    return addPersonaNode(root->next,p);
 }
 
 // Initializes a Database at NULL values
@@ -106,25 +154,6 @@ Database * crateDatabase(){
     return db;
 }
 
-void freeNodes(IndexNode* node){
-    if (node == NULL){
-        return;
-    }
-    freeNodes(node->left);
-    freeNodes(node->right);
-    free(node->data);
-    free(node);
-}
-
-void freeDatabase(Database* database){
-    freeNodes(database->address);
-    freeNodes(database->age);
-    freeNodes(database->name);
-    freeNodes(database->surname);
-    free(database);
-}
-
-
 // Inserts a node in a orderly manner.
 IndexNode* insertNode(IndexNode* root, DataStructure* dato, Persona* p){
     if (root == NULL){
@@ -132,9 +161,13 @@ IndexNode* insertNode(IndexNode* root, DataStructure* dato, Persona* p){
         //mio debug, non ci faccia caso che prima stava esplodendo tutto e volevo esplodere anche io
         return createNode(dato, p);
     }
-    if (dataCompare(root->data, dato) > 0){ // If this is >0, it means that the value of the current node is greater, so our own is placed left
+    if (dataCompare(root->data, dato->value) > 0){ // If this is >0, it means that the value of the current node is greater, so our own is placed left
         //printf("Inserimento Sinistro- %d\n", dato->value);
         root->left = insertNode(root->left, dato, p);
+        return root;
+    }
+    if (dataCompare(root->data, dato->value) == 0){
+        root->records = addPersonaNode(root->records, p);
         return root;
     }
     //printf("Inserimento Destro- %d\n", dato->value);
@@ -149,6 +182,100 @@ void insert(Database * database, Persona * persona){ //Si, mi piace l'OOP! Come 
     database->surname = insertNode(database->surname, datafy((Dato)persona->surname, STRING), persona);
 }
 
+
+/***
+ *      __  __                                   ______             _             
+ *     |  \/  |                                 |  ____|           (_)            
+ *     | \  / | ___ _ __ ___   ___  _ __ _   _  | |__ _ __ ___  ___ _ _ __   __ _ 
+ *     | |\/| |/ _ \ '_ ` _ \ / _ \| '__| | | | |  __| '__/ _ \/ _ \ | '_ \ / _` |
+ *     | |  | |  __/ | | | | | (_) | |  | |_| | | |  | | |  __/  __/ | | | | (_| |
+ *     |_|  |_|\___|_| |_| |_|\___/|_|   \__, | |_|  |_|  \___|\___|_|_| |_|\__, |
+ *                                        __/ |                              __/ |
+ *                                       |___/                              |___/ 
+ */
+
+void freePersonaNode(PersonaNode* node){
+    if (node == NULL){
+        return;
+    }
+    freePersonaNode(node->next);
+    free(node);
+}
+
+void freeNodes(IndexNode* node){
+    if (node == NULL){
+        return;
+    }
+    freeNodes(node->left);
+    freeNodes(node->right);
+    freePersonaNode(node->records);
+    free(node->data);
+    free(node);
+}
+
+void freeDatabase(Database* database){
+    freeNodes(database->address);
+    freeNodes(database->age);
+    freeNodes(database->name);
+    freeNodes(database->surname);
+    free(database);
+}
+
+
+
+/***
+ *      _____                        _       ______ _           _ _             
+ *     |  __ \                      | |     |  ____(_)         | (_)            
+ *     | |__) |___  ___ ___  _ __ __| |___  | |__   _ _ __   __| |_ _ __   __ _ 
+ *     |  _  // _ \/ __/ _ \| '__/ _` / __| |  __| | | '_ \ / _` | | '_ \ / _` |
+ *     | | \ \  __/ (_| (_) | | | (_| \__ \ | |    | | | | | (_| | | | | | (_| |
+ *     |_|  \_\___|\___\___/|_|  \__,_|___/ |_|    |_|_| |_|\__,_|_|_| |_|\__, |
+ *                                                                         __/ |
+ *                                                                        |___/ 
+ */
+PersonaNode* findByValue(IndexNode* node, Dato dato){
+    if(node == NULL){
+        return NULL;
+    }
+    if (dataEquals(node->data, dato)){
+        return node->records;
+    }
+    if (dataCompare(node->data, dato) > 0){
+        return findByValue(node->left, dato);
+    }
+    return findByValue(node->right, dato);
+
+}
+
+// Please note: each of those functions will return only the first Persona to match in order to maintain compatibility
+Persona* findByName(Database * database, char * name){
+    return findByValue(database->name, (Dato)name)->persona;
+};
+Persona* findBySurname(Database * database, char * surname){
+    return findByValue(database->surname, (Dato)surname)->persona;
+};
+Persona* findByAddress(Database * database, char * address){
+    return findByValue(database->address, (Dato)address)->persona;
+};
+Persona* findByAge(Database * database, int age){
+    return findByValue(database->age, (Dato)age)->persona;
+};
+
+
+
+
+
+
+/***
+ *      _____       _                 
+ *     |  __ \     | |                
+ *     | |  | | ___| |__  _   _  __ _ 
+ *     | |  | |/ _ \ '_ \| | | |/ _` |
+ *     | |__| |  __/ |_) | |_| | (_| |
+ *     |_____/ \___|_.__/ \__,_|\__, |
+ *                               __/ |
+ *                              |___/ 
+ */
 void inOrderVisit(IndexNode* node){
     if (node != NULL){
         inOrderVisit(node->left);
@@ -157,36 +284,8 @@ void inOrderVisit(IndexNode* node){
     }
 }
 
-Persona* findByValue(IndexNode* node, Dato dato){
-    if(node == NULL){
-        return NULL;
-    }
-    if (dataEquals(node->data, dato)){
-        return node->record;
-    }
-    if (dataCompareRaw(node->data, dato) > 0){
-        return findByValue(node->left, dato);
-    }
-    return findByValue(node->right, dato);
-
-}
-
-Persona* findByName(Database * database, char * name){
-    return findByValue(database->name, (Dato)name);
-};
-Persona* findBySurname(Database * database, char * surname){
-    return findByValue(database->surname, (Dato)surname);
-};
-Persona* findByAddress(Database * database, char * address){
-    return findByValue(database->address, (Dato)address);
-};
-Persona* findByAge(Database * database, int age){
-    return findByValue(database->age, (Dato)age);
-};
-
-
-/*
 Persona p1 = {"Mario", "Rossi", "Mushroom Kingdom, 10", 23};
+Persona p4 = {"Mario2LaVendetta", "Rossi", "Mushroom Kingdom, 10", 23};
 Persona p2 = {"Wario", "Gialli", "Mushroom Kingdom, 15", 22};
 Persona p3 = {"Luigi", "Verdi", "Mushroom Kingdom, 12", 21};
 
@@ -196,12 +295,13 @@ int main(){
     insert(db, &p1);
     insert(db, &p2);
     insert(db, &p3);
+    insert(db, &p4);
     inOrderVisit(db->surname);
-    //printf("%s", findByAge(db, 21)->name);
+    printf("%s", findByValue(db->age, (Dato)23)->persona->name);
     freeDatabase(db);
-    inOrderVisit(db->surname);
+    //inOrderVisit(db->surname);
     return 0;
 };
-*/
+
 
 
